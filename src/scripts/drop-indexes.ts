728c4da651d2465d8a0a8f171811f68ee9b7f5cb
Jonn Nilsson <jonn@jonn.nu>
2021-03-20 20:33:11 +0100

    fix: add script to drop all indexes in db

---
+++ src/scripts/drop-indexes.ts
@@ -0,0 +1,24 @@
+import { Db } from "mongodb";
+import { Logger } from "@lindorm-io/winston";
+import { MONGO_CONNECTION_OPTIONS } from "../config";
+import { mongoQuery } from "@lindorm-io/mongo";
+import { winston } from "../logger";
+
+(async () => {
+  try {
+    await mongoQuery(
+      { logger: winston, mongoOptions: MONGO_CONNECTION_OPTIONS },
+      async ({ db }: { db: Db; logger: Logger }) => {
+        const collections = await db.collections();
+
+        for (const collection of collections) {
+          await collection.dropIndexes();
+        }
+      },
+    );
+  } catch (err) {
+    winston.error("error", err);
+  } finally {
+    process.exit(0);
+  }
+})();
